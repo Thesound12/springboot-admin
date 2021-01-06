d7cc7e5da0b1b21a541c5a8ca2c0aec3dacb4a31
Thesound <Thesound@qq.com>
2021-01-06 21:03:33 +0800

    springbootadmin   1.5.7版本

---
+++ srpingbootadminclient/src/main/java/com/bjfu/AdminClientApplication_8768.java
@@ -0,0 +1,11 @@
+package com.bjfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminClientApplication_8768 {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClientApplication_8768.class, args);
+    }
+}
